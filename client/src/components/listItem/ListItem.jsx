57b39a355c288267e009a74b161c04059088c887
Andy Yang <yangandy1001@gmail.com>
2022-05-28 22:40:44 -0400

    working on adding info to list items when hovered over

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -1,9 +1,24 @@
 import "./ListItem.scss"
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
 
 function ListItem() {
   return (
     <div className="listItem">
-        item
+        <img src="https://images6.alphacoders.com/684/684831.jpg" alt=""/>
+    
+
+      <div className="itemInfo">
+        <div className="icons">
+            <PlayArrowIcon/>
+            <AddIcon/>
+            <ThumbUpOutlinedIcon/>
+            <ThumbDownOutlinedIcon/>
+        </div>
+      </div>
+
     </div>
   )
 }

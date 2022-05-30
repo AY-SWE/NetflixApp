74ec3d943d566a17f1af384d788784b906b5a196
Andy Yang <yangandy1001@gmail.com>
2022-05-30 16:52:34 -0400

    finished register screen component, working on login screen

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,23 @@
+import "./Login.scss";
+import {useState, useRef} from "react";
+
+function Login() {
+  
+  return (
+    <div className="login">
+        <div className="top">
+            <div className="wrapper">
+              <img  className="logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
+              
+            </div>
+        </div>
+
+        <div className="container">
+            
+        </div>
+    </div>
+    
+  )
+}
+
+export default Login
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,20 @@
 import "./Register.scss";
+import {useState, useRef} from "react";
 
 function Register() {
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  const emailRef = useRef();
+  const passRef = useRef();
+  
+  const handleStart = () =>{
+    setEmail(emailRef.current.value);
+  }
+  const handleFinish = () =>{
+    setEmail(passRef.current.value);
+  }
+
+  
   return (
     <div className="register">
         <div className="top">
@@ -13,11 +27,19 @@ function Register() {
         <div className="container">
             <h1>Unlimited films, TV shows, and more.</h1>
             <h2>Watch anywhere. Cancel anytime</h2>
-            <div className="input">
-                <input type="email" className="type" placeholder="Email address"/>
-                <button className="registerButton">TRY IT NOW</button>
+            <p>Ready to watch? Enter your email to create orart paid membership</p>
+            
+            {!email ? (
+            <div className="input"> st
+                <input type="email" placeholder="Email address" ref={emailRef}/>
+                <button className="registerButton" onClick={handleStart}>Get Started</button>
             </div>
-            <p>Ready to watch? Enter your email to create or start paid membership</p>
+            ) : (
+            <form className="input"> st
+              <input type="password" placeholder="Password" ref={passRef}/>
+              <button className="registerButton" onClick={handleFinish}>Start</button>
+            </form>
+        )}
         </div>
     </div>
     

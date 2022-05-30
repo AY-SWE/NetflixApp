56cbcd3481cbb3a2ff828b110135b95244bb4747
Andy Yang <yangandy1001@gmail.com>
2022-05-30 15:08:13 -0400

    fixed the white borders on each side, body has 8 margin by default

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -4,8 +4,10 @@ function Register() {
   return (
     <div className="register">
         <div className="top">
-            <img src="" alt="" className="logo" />
-            <button className="loginButton">Sign in</button>
+            <div className="wrapper">
+              <img  className="logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
+              <button className="loginButton">Sign in</button>
+            </div>
         </div>
 
         <div className="container">
